66fccffeb4a4c9598a733a26762cca8aed6f9dca
mhpham23 <mhpham23@vt.edu>
2023-09-19 15:01:27 -0400

    Create SDFile.ino

---
+++ SDFile/SDFile.ino
@@ -0,0 +1,102 @@
+/*
+  SD card datalogger
+ 
+ This example shows how to log data from three analog sensors
+ to an SD card using the SD library.
+ 	
+ The circuit:
+ * analog sensors on analog ins 0, 1, and 2
+ * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11, pin 7 on Teensy with audio board
+ ** MISO - pin 12
+ ** CLK - pin 13, pin 14 on Teensy with audio board
+ ** CS - pin 4,  pin 10 on Teensy with audio board
+ 
+ */
+
+#include <SD.h>
+#include <SPI.h>
+
+// On the Ethernet Shield, CS is pin 4. Note that even if it's not
+// used as the CS pin, the hardware CS pin (10 on most Arduino boards,
+// 53 on the Mega) must be left as an output or the SD library
+// functions will not work.
+
+// change this to match your SD shield or module;
+// Arduino Ethernet shield: pin 4
+// Adafruit SD shields and modules: pin 10
+// Sparkfun SD shield: pin 8
+// Teensy audio board: pin 10
+// Teensy 3.5 & 3.6 & 4.1 on-board: BUILTIN_SDCARD
+// Wiz820+SD board: pin 4
+// Teensy 2.0: pin 0
+// Teensy++ 2.0: pin 20
+const int chipSelect = BUILTIN_SDCARD;
+void setup_SD(){
+  //Comment out for integration/////////////////////////////
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect.
+  }
+  ///////////////////////////////////////////////////////////
+
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    while (1) {
+      // No SD card, so don't do anything more - stay stuck here
+    }
+  }
+  Serial.println("card initialized.");
+}
+
+void setup()
+{
+  setup_SD();
+
+}
+
+void writeToSD()
+{
+  // make a string for assembling the data to log:
+  String dataString = "Test ";
+  //int sensor = analogRead(analogPin);
+  //dataString += String(sensor);
+
+  // open the file.
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(dataString);
+  } else {
+    // if the file isn't open, pop up an error:
+    Serial.println("error opening datalog.txt");
+  }
+  delay(100); // run at a reasonable not-too-fast speed
+  
+}
+void loop()
+{
+  uint8_t counter = 0;
+  if (counter < 1)
+  {
+    writeToSD();
+  }
+  else {
+  {
+    Serial.println("finished writing 1 test to SD");
+  }
+  }
+}
+
+
+
+
+
